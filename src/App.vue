a94367b3fa1be230a973bb845a80a539b63e6d8f
Dmitry <DmitrySavenoks@gmail.com>
2017-04-26 19:15:19 +0300

    some stuff

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,64 @@
       <div class="header">
         <div class="content">
           <div class="menu">
-            <a href="#">Home</a>
-            <a href="#">About us</a>
-            <a href="#">Contacts</a>
+            <a href="#"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Home</a>
+            <a href="#"><i class="fa fa-question fa-fw" aria-hidden="true"></i>&nbsp; About us</a>
+            <a href="#"><i class="fa fa-address-book-o fa-fw" aria-hidden="true"></i>&nbsp; Contacts</a>
           </div>
         </div>
       </div>
       <div class="main">
         <div class="content">
           
-          <div class="item-list-1"></div>
-          <div class="item-list-2"></div>
+          <div class="row columns-2">
+            <div class="column">
+              <div class="item-list-pic">
+                <img src="https://unsplash.it/500/300/?random">
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="item-list item-list-1">
+                <h3>Something here</h3>
+                <p>Paragraph here:</p>
+                <ul>
+                  <li><i class="fa fa-bed" aria-hidden="true"></i>&nbsp; Furniture</li>
+                  <li><i class="fa fa-industry" aria-hidden="true"></i>&nbsp; Industry</li>
+                  <li><i class="fa fa-tv" aria-hidden="true"></i>&nbsp; TVs n stuff</li>
+                  <li><i class="fa fa-bar-chart" aria-hidden="true"></i>&nbsp; Office stuff</li>
+                  <li><i class="fa fa-truck" aria-hidden="true"></i>&nbsp; just stuff</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="row columns-2">
+            <div class="column">
+              <div class="item-list-pic">
+                <img src="https://unsplash.it/500/300/?random">
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="item-list item-list-2">
+                <h3>Something here</h3>
+                <p>Paragraph here:</p>
+                <ul>
+                  <li><i class="fa fa-bed" aria-hidden="true"></i>&nbsp; Furniture</li>
+                  <li><i class="fa fa-industry" aria-hidden="true"></i>&nbsp; What</li>
+                  <li><i class="fa fa-tv" aria-hidden="true"></i>&nbsp; What</li>
+                  <li><i class="fa fa-bar-chart" aria-hidden="true"></i>&nbsp; What</li>
+                  <li><i class="fa fa-truck" aria-hidden="true"></i>&nbsp; What</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            
+            <PriceTable></PriceTable>
+
+          </div>
 
         </div>
       </div>
@@ -27,17 +74,26 @@
 </template>
 
 <script>
+
+import PriceTable from './components/PriceTable.vue';
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    PriceTable
   }
 }
 </script>
 
 <style lang="stylus">
+
+@import 'assets/css/font-awesome.min.css'
+
 
 archBlue       = #489fdf
 archGreen      = #1fa67a
@@ -46,24 +102,38 @@ archLightGreen = #34a782
 archGrey       = #f5f5f5
 archDarkGrey   = #333
 
+fontSerif      = 'PT Serif', Georgia, 'Times New Roman', serif
+fontSansSerif  = 'Open Sans', Arial, sans-serif
 
 html
 body
   margin 0px
   padding 0px
+  
+// div
+//   box-shadow 0 0 5px 2px indianred
+  
 
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
+  font-family fontSansSerif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
   color #2c3e50
-  // margin-top 60px
   
+/* positioning */
+
+.wrapper
+  .columns-2
+    .column
+      display inline-block
+      width 50%
+      min-height 400px
+      vertical-align top
 
 .wrapper
   width 100%
   height 100%
+  white-space nowrap
   
   .content
     position relative
@@ -74,8 +144,17 @@ body
     right 0px
     margin auto
     
-
-    box-shadow 0 0 5px 2px indianred
+    .item-list
+      padding-left 50px
+      h3
+        font-size 32px
+      ul
+        li
+          i
+            margin-right 10px
+    .item-list-pic
+      text-align center
+      margin-top 40px
     
   .header
     background-color archBlue
@@ -90,6 +169,7 @@ body
       right 0px
       box-shadow 0 0 3px 0px archDarkGrey
       border-radius 0px 0px 2px 2px
+      text-align center
       z-index 99
       a
         flex 1 0 33%
@@ -108,16 +188,16 @@ body
      
 
 
-h1, h2
-  font-weight normal
+h1, h2, h3
+  font-weight bold
+  font-family fontSerif
 
 ul
   list-style-type none
   padding 0
 
 li
-  display inline-block
-  margin 0 10px
+  margin 15px 20px
 
 a
   // color #42b983
